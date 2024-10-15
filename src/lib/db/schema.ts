f4872ef6e7a707e44e2b33f8e1096265d44b1c02
Allan Lasser <lasser.allan@gmail.com>
2024-10-15 12:16:16 -0400

    Adds relations between Users and Tools

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from 'drizzle-orm';
+import { sql, relations } from 'drizzle-orm';
 import { text, pgTable, uuid, serial } from 'drizzle-orm/pg-core';
 
 export const Users = pgTable('users', {
@@ -12,11 +12,23 @@ export const Users = pgTable('users', {
 	ordinal: serial('ordinal').notNull() // your user number, for fun
 });
 
+export const UsersRelations = relations(Users, ({ many }) => ({
+	tools: many(Tools)
+}));
+
 export type User = typeof Users.$inferSelect;
 
 export const Tools = pgTable('tools', {
 	id: uuid('id')
 		.notNull()
 		.default(sql`gen_random_uuid()`), // Generate a UUID by default
-	name: text('name').notNull()
+	name: text('name').notNull(),
+	ownerId: uuid('owner_id')
 });
+
+export const ToolsRelations = relations(Tools, ({ one }) => ({
+	owner: one(Users, {
+		fields: [Tools.ownerId],
+		references: [Users.id]
+	})
+}));
