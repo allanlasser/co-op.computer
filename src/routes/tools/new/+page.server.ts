e911b59aba7665b43dbc9a9b0c6eae4d9703b752
Allan Lasser <lasser.allan@gmail.com>
2024-10-15 13:07:01 -0400

    Create new tool and save current user as owner

---
+++ src/routes/tools/new/+page.server.ts
@@ -0,0 +1,17 @@
+import { fail } from '@sveltejs/kit';
+import { createTool } from '$lib/db/tools';
+
+export const actions = {
+	default: async ({ request, locals }) => {
+		const user = locals.session?.user;
+		if (!user) {
+			return fail(403, {
+				errors: 'You must be signed in to add a new tool.'
+			});
+		}
+		const data = await request.formData();
+		const name = String(data.get('name'));
+		const newTool = await createTool({ name, ownerId: user.id });
+		return { success: true, data: { ...newTool } };
+	}
+};
---
+++ src/routes/tools/new/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+
+	export let form;
+
+	$: if (form?.success) {
+		// show a success message
+		goto('/tools');
+	}
+</script>
+
+<div class="page">
+	<h1>Add a Tool</h1>
+	<form method="POST" use:enhance>
+		<label>
+			Name
+			<input name="name" type="name" />
+		</label>
+		<button>Add tool</button>
+	</form>
+	{#if form?.success}
+		<p>Tool added!</p>
+	{/if}
+</div>
+
+<style>
+	.page {
+		margin: calc(4 * var(--unit)) auto;
+		max-width: 64rem;
+	}
+	form {
+		margin: calc(2 * var(--unit)) 0;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		gap: calc(2 * var(--unit));
+	}
+</style>
