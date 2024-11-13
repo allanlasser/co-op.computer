9c8d098b9a5a32f46c1aea2948f9d62ca6182b56
Allan Lasser <lasser.allan@gmail.com>
2024-11-12 22:30:18 -0500

    Handle errors on SignIn form

---
+++ src/lib/components/forms/SignIn.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Field from '../ui/Field.svelte';
+
+	export let errors: Record<string, string[]> = {};
+
+	let email = '';
+	let password = '';
+</script>
+
+<form method="POST">
+	<Field label="Email" errors={errors.username}>
+		<input type="email" name="email" bind:value={email} />
+	</Field>
+	<Field label="Password" errors={errors.password}>
+		<input type="password" name="password" bind:value={password} />
+	</Field>
+	<footer>
+		<button type="submit">Sign In</button>
+	</footer>
+</form>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: calc(2 * var(--unit));
+	}
+</style>
